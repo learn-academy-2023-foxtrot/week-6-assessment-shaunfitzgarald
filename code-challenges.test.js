58062036e3631ff4cfc52548d2958e0587d3633e
Shaun Stephenson <shaunfitzgarald@gmail.com>
2023-10-15 13:50:59 -0700

    Worked on assignment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
+const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,20 +16,51 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
+describe("objArrToSentencesArr", () => {
+  it ("takes in an array of objects and returns an array with a sentence about each person with their name capitalized", () => {
+  expect(objArrToSentencesArr(hitchhikersCharacters)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+})
+})
+
 const hitchhikersCharacters = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
+  { name: "arthur dent", occupation: "a radio employee" }]
+
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 // b) Create the function that makes the test pass.
 
-// Pseudo code:
+/* Pseudo code:
+name: objArrToSentencesArr
+tools:
+posiible too string interpolation
+possibly use .toUppercase
+possiby for lopp or .map to go through the array
+goal: is go through the array pull out the name and occupation and return each instance with it's own sentence string interpolated and with the names capitalized
+input: is an array of objets
+output: An array of sentences
+process: map throught the array, split the name at the space, capitalized the first letter of each name, join the firs letter of each name bacj=k to the rest of the name, and interpolate the name and occupation into a sentence
+*/
+const objArrToSentencesArr = (array) => {
+  return array.map((value) => 
+   `${value.name.split(" ").map(name =>  
+   name.charAt(0).toUpperCase() + name.slice(1)).join(" ")} is ${value.occupation}.`
+   )
+}
+// console.log(objArrToSentencesArr(hitchhikersCharacters))
+
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
+
+describe("modulosThree", () => {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    expect(modulosThree(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(modulosThree(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
 
 const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
@@ -37,11 +70,32 @@ const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+//name: modulosThree
+//tools: .filter, %, typeof
+//goal: to return an array of only the REMAINDERS of the numbers when divided by 3.
+//input: mixed data array
+//output: an array of only the remainders of the numbers when divided by 3
+//process:
+//use .filter to go through the array and return only the numbers
+//use % to get the remainder of the numbers when divided by 3
+//return the array of remainders
+
+const modulosThree = (array) => {
+  return array.filter(value => typeof value === "number").map(value => value % 3)
+}
+
+
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
+describe("cubeAndSum", () => {
+  it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+    expect(cubeAndSum(cubeAndSum1)).toEqual(99)
+    expect(cubeAndSum(cubeAndSum2)).toEqual(1125)
+  })
+})  
 const cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 const cubeAndSum2 = [0, 5, 10]
@@ -50,3 +104,19 @@ const cubeAndSum2 = [0, 5, 10]
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+//name: cubeAndSum
+//tools: .map, .reduce, Math.pow or **
+//goal: to return the sum of all the numbers cubed
+//input: an array of numbers
+//output: a number
+//process:
+//use .map to go through the array and cube each number
+//use .reduce to add all the cubed numbers together
+//return the sum of all the cubed numbers
+
+const cubeAndSum = (array) => {
+  return array.map(value => Math.pow(value, 3)).reduce((acc, value) => acc + value)
+}
+
+
+
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,39 +6,38 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1)This line is estabishing the BlogPostsController class as a child of the ApplicationController class. This is done so that the BlogPostsController class can inherit all of the methods and attributes of the ApplicationController class.
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2)This line is creating an instance variable called @posts that is equal to all of the BlogPost objects in the database.
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3)This line is creating a method called show that will find a specific BlogPost object in the database based on the id that is passed in as a parameter. The method will then create an instance variable called @post that is equal to the BlogPost object that was found.
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4)This line is creating a method called new that will create a new BlogPost object.
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
-    @post = BlogPost.create(blog_post_params)
+    # ---5)This line is creating a new BlogPost object and passing in the blog_post_params method as a parameter. The blog_post_params method is a private method that is defined at the bottom of the file. 
     if @post.valid?
       redirect_to blog_post_path(@post)
     end
   end
 
   def edit
-    # ---6)
+    # ---6)This line is creating an instance variable called @post that is equal to a specific BlogPost object in the database based on the id that is passed in as a parameter.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7)This line is updating the BlogPost object that was found in the edit method with the parameters that are passed in from the blog_post_params method. The blog_post_params method is a private method that is defined at the bottom of the file. 
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -48,15 +47,15 @@ class BlogPostsController < ApplicationController
   def destroy
     @post = BlogPost.find(params[:id])
     if @post.destroy
-      # ---8)
+      # ---8)This line is redirecting the user to the index page of the blog_posts controller.
       redirect_to blog_posts_path
     end
   end
 
-  # ---9)
+  # ---9)This line is creating a private method called blog_post_params that will return the parameters that are passed in from the create and update methods. The private method is used to prevent the method from being called outside of the class.
   private
   def blog_post_params
-    # ---10)
+    # ---10)This line is requiring the blog_post parameter and permitting the title and content parameters to be passed in.
     params.require(:blog_post).permit(:title, :content)
   end
 end
